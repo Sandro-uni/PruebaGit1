a6071c7f23de256518145fccaa3d562be3b6ac02
sandro <sandro.llavilla.a@uni.pe>
2024-09-28 02:57:56 -0500

    guardado1 ventana boton cambiar texto

---
+++ src/main/java/com/mycompany/gitprueba1/GitPrueba1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitprueba1;
+import view.ventana;
+
+/**
+ *
+ * @author San
+ */
+public class GitPrueba1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        ventana venti=new ventana();
+        venti.setVisible(true);
+    }
+}
